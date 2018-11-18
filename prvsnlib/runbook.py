786349a085021ddab9ed54790355938ef0ad2205
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-17 23:52:07 -0500

    Using dedent for heredoc

--- prvsnlib/runbook.py
+++ prvsnlib/runbook.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 
 from .role import Role
 from .utils.file import mkdir_p
@@ -53,27 +54,29 @@ class Runbook:
 
         main_base_roles_path = os.path.join(base_roles_path, 'main.py')
         with open(main_base_roles_path, 'w') as f:
-            f.write('''
-# This is a template for a role
+            data = textwrap.dedent('''
+                # This is a template for a role
 
-bash('echo "hello"')
+                bash('echo "hello"')
 
-package('pkill')
+                package('pkill')
 
-file(
-    'yo.conf', 
-    '/etc/yo.conf', 
-    replacements={
-        'USERNAME': 'arnaud'
-    },
-)
-            ''')
+                file(
+                    'yo.conf', 
+                    '/etc/yo.conf', 
+                    replacements={
+                        'USERNAME': 'arnaud'
+                    },
+                )
+            ''').strip()
+            f.write(data)
 
         files_base_roles_path = os.path.join(base_roles_path, 'files')
         mkdir_p(files_base_roles_path)
 
         with open(os.path.join(files_base_roles_path, 'yo.conf'), 'w') as f:
-            f.write('''
-username = USERNAME
-dob = 01/01/1970
+            data = textwrap.dedent('''
+                username = USERNAME
+                dob = 01/01/1970
             ''')
+            f.write(data)
