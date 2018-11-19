e382337e3d71c9a2ccd88c280766a4f75e8ba3a0
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-18 19:41:55 -0500

    Added log_error_and_exit in validation

--- prvsnlib/utils/validation.py
+++ prvsnlib/utils/validation.py
@@ -2,20 +2,30 @@ import logging
 import os
 import sys
 
+from prvsnlib.runbook import Runbook
+
+
+def log_error_and_exit(message):
+    logging.error(message)
+    sys.exit(1)
+
 
 def validate_runbook(runbook):
-    if os.path.isdir(runbook._path):
-        return True
-    else:
-        logging.error('Argument "' + runbook._path + '" is not a valid runbook path.')
-        sys.exit(1)
+    if not type(runbook) is Runbook:
+        log_error_and_exit('Argument runbook is not a valid Runbook.')
+    if not os.path.isdir(runbook._path):
+        log_error_and_exit('Argument "' + runbook._path + '" is not a valid runbook path.')
+    return True
+
 
 def validate_roles(runbook, roles):
+    if not type(runbook) is Runbook:
+        log_error_and_exit('Argument runbook is not a valid Runbook.')
+    if not type(roles) is str:
+        log_error_and_exit('Argument "' + roles + '" is not a valid roles list.')
     if not roles:
-        logging.error('No roles specified. Nothing to do.')
-        sys.exit(1)
+        log_error_and_exit('No roles specified. Nothing to do.')
     for role in roles.split(','):
         if role not in [r.name for r in runbook.roles]:
-            logging.error('Argument "' + role + '" is not a valid role in runbook.')
-            sys.exit(1)
+            log_error_and_exit('Argument "' + role + '" is not a valid role in runbook.')
     return True
