d35708b5edafde0471a4b1c01ba19bfff76f534e
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-18 19:48:45 -0500

    Removed type check from validation (was not working under 2.7)

--- prvsnlib/utils/validation.py
+++ prvsnlib/utils/validation.py
@@ -11,16 +11,12 @@ def log_error_and_exit(message):
 
 
 def validate_runbook(runbook):
-    if not type(runbook) is Runbook:
-        log_error_and_exit('Argument runbook is not a valid Runbook.')
     if not os.path.isdir(runbook._path):
         log_error_and_exit('Argument "' + runbook._path + '" is not a valid runbook path.')
     return True
 
 
 def validate_roles(runbook, roles):
-    if not type(runbook) is Runbook:
-        log_error_and_exit('Argument runbook is not a valid Runbook.')
     if not type(roles) is str:
         log_error_and_exit('Argument "' + roles + '" is not a valid roles list.')
     if not roles:
