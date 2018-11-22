f72764e5423aa443e27a0c6812886a2188e55986
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-21 21:03:01 -0500

    Added chown to file

--- prvsnlib/tasks/file.py
+++ prvsnlib/tasks/file.py
@@ -1,4 +1,5 @@
 import os
+import pwd
 
 from prvsnlib.utils.file import mkdir_p, get_replace_write_file
 from ..task import Task, TaskResult
@@ -10,10 +11,11 @@ class FileAction:
 
 class FileTask(Task):
 
-    def __init__(self, source, dest, replacements, action, secure):
+    def __init__(self, source, dest, owner, replacements, action, secure):
         Task.__init__(self, secure)
         self._source = source
         self._dest = dest
+        self._owner = owner
         self._action = action
         self._replacements = replacements
 
@@ -26,10 +28,18 @@ class FileTask(Task):
                                       os.path.join(self._role.path, 'files'),
                                       self._replacements,
                                       self._dest)
+
+        try:
+            uid = pwd.getpwnam(self._owner).pw_uid if self._owner else -1
+            gid = pwd.getgrnam(self._group).gr_gid if self._group else -1
+            os.chown(self._dest, uid, gid)
+        except OSError as e:
+            err.append('Could not change file ownership.')
+
         return TaskResult(output=out, error=err)
 
 
-def file(source, dest, replacements=None, action=FileAction.ADD, secure=False):
+def file(source, dest, replacements=None, owner=None, action=FileAction.ADD, secure=False):
     if replacements is None:
         replacements = {}
     FileTask(source, dest, replacements, action, secure)
