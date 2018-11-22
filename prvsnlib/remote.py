14e665e03c9fabb3d5a4abb9fcf63a7aa593218e
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-21 19:27:48 -0500

    Streaming ssh

--- prvsnlib/remote.py
+++ prvsnlib/remote.py
@@ -18,25 +18,91 @@ class Remote:
         self._package = package
         self._sudo = sudo
 
-    def run(self):
-
-        ssh = Ssh(remote=self._hostname, user=self._username)
-
+    def manage_public_keys(self, ssh):
         if not self._no_copy_keys:
+            logging.header('Copying public keys to ' + self._hostname)
             ssh.copy_public_keys()
 
-        dest = 'package.pyz'
+            all_lines = []
+            for line in ssh.output:
+                logging.debug(line)
+                all_lines.append(line)
 
-        logging.header('Sending package to ' + ssh.remote)
-        out, err = ssh.copy_to(self._package, dest)
-        if err:
+            if 'skipped because they already exist' in ''.join(all_lines):
+                logging.success('Public keys already present. Skipping.')
+            else:
+                if ssh.returncode is None:
+                    pass
+                elif ssh.returncode == 0:
+                    logging.success('Public keys copied.')
+                elif ssh.returncode > 0:
+                    logging.error('Could not copy public key')
+
+
+    def copy_package(self, ssh):
+        logging.header('Sending package to ' + ssh.hostname)
+        ssh.copy_to(self._package, self._package)
+
+        for line in ssh.output:
+            logging.debug(line)
+
+        exit_code = 0
+
+        if ssh.error:
+            for line in ssh.error:
+                logging.error(line)
+                exit_code = 1
+
+        if ssh.returncode is None:
+            pass
+        elif ssh.returncode == 0:
+            logging.debug('return code: 0')
+        elif ssh.returncode > 0:
+            logging.error('return code: ' + str(ssh.returncode))
+            exit_code = 1
+
+        if exit_code:
             logging.error('Sending package failed.')
-            sys.exit(1)
+            sys.exit(exit_code)
+
         logging.success('Sent.')
 
-        logging.header('Remotely executing package on ' + ssh.remote)
-        out, err = ssh.run_command(['python', dest], log_level=logging.INFO, sudo=self._sudo)
-        if err:
+    def execute_package(self, ssh):
+        logging.header('Remotely executing package on ' + ssh.hostname)
+
+        ssh.run_command(['python', self._package], sudo=self._sudo)
+
+        if ssh.output:
+            for line in ssh.output:
+                logging.info(line)
+
+        exit_code = 0
+
+        if ssh.error:
+            for line in ssh.error:
+                logging.error(line)
+                exit_code = 1
+
+        if ssh.returncode is None:
+            pass
+        elif ssh.returncode == 0:
+            logging.debug('return code: 0')
+        elif ssh.returncode > 0:
+            logging.error('return code: ' + str(ssh.returncode))
+            exit_code = 1
+
+        if exit_code:
             logging.error('Remotely executing package failed.')
-            sys.exit(1)
-        logging.success('Executed.')
+            sys.exit(exit_code)
+
+        logging.success('Remote package executed.')
+
+    def run(self):
+
+        ssh = Ssh(hostname=self._hostname, username=self._username)
+
+        self.manage_public_keys(ssh)
+        self.copy_package(ssh)
+        self.execute_package(ssh)
+
+        logging.success('Remote provisioned.')
--- prvsnlib/utils/ssh.py
+++ prvsnlib/utils/ssh.py
@@ -3,72 +3,29 @@ import logging
 import pty
 import os
 
-from prvsnlib.utils.run import run
+# from prvsnlib.utils.run import run
 from prvsnlib.utils.file import mkdir_p
+
+CHILD = 0
+
 
 
 class Ssh:
 
-    def __init__(self, remote='localhost', user=getpass.getuser(), password=None):
-        self.hostname = remote
-        self.username = user
+    def __init__(self, hostname='localhost', username=getpass.getuser(), password=None):
+        self.hostname = hostname
+        self.username = username
         self._password = password
+
+        self._commands = None
+        self._pid = None
+        self._child_fd = None
+        self._returncode = None
+        self._exception = None
+        self._error = None
 
     def __str__(self):
         return '<SSH ' + self.username + '@' + self.hostname + '>'
-
-    @property
-    def remote(self):
-        return self.hostname + '@' + self.username
-
-    def run_command(self, commands, log_level=logging.DEBUG, sudo=False):
-        commands_to_run = [
-            '/usr/bin/ssh',
-            '-t',
-            self.username + '@' + self.hostname,
-        ]
-
-        if sudo:
-            commands_to_run += ['sudo', '-p "sudo password: "']
-
-        commands_to_run += commands
-
-        return self.run(
-            commands_to_run,
-            log_level=log_level
-        )
-
-    def copy_to(self, src, dest, log_level=logging.DEBUG):
-        out1 = ''
-        err1 = ''
-        if os.path.dirname(dest):
-            out1, err1 = self.run([
-                '/usr/bin/ssh',
-                self.username + '@' + self.hostname,
-                'mkdir -p ' + os.path.dirname(dest),
-            ],
-                log_level=log_level
-            )
-        if err1:
-            return out1, err1
-        out2, err2 = self.run([
-            '/usr/bin/scp',
-            src,
-            self.username + '@' + self.hostname + ':' + dest,
-        ],
-            log_level=log_level
-        )
-        return out1 + '\n' + out1, err1 + err2
-
-    def copy_from(self, src, dest, log_level=logging.DEBUG):
-        mkdir_p(os.path.dirname(dest))
-        return self.run([
-            '/usr/bin/scp',
-            self.username + '@' + self.hostname + ':' + src,
-            dest,
-        ],
-            log_level=log_level,
-        )
 
     @property
     def ssh_config_dir(self):
@@ -92,7 +49,7 @@ class Ssh:
             mkdir_p(config_dir)
             os.chmod(config_dir, 0o700)
             logging.info('No public key found; creating a new key at "' + self.public_key_file + '"')
-            run([
+            self.run([
                 'ssh-keygen',
                 '-t', 'rsa',
                 '-b', '4096',
@@ -101,56 +58,93 @@ class Ssh:
             ])
 
     def copy_public_keys(self):
-        logging.header('Copying public keys to ' + self.hostname)
-
         self.create_public_key_file_if_not_exist()
 
-        out, err = self.run([
+        return self.run([
             '/usr/bin/ssh-copy-id',
             self.username + '@' + self.hostname,
-        ],
-        )
-        if 'skipped because they already exist' in out:
-            logging.success('Public keys already present. Skipping.')
-        elif err:
-            logging.error('Could not copy public key')
-        else:
-            logging.success('Public keys copied.')
-        return out, err
+        ])
 
-    def run(self, commands, log_level=logging.DEBUG):
+    def run_command(self, commands, sudo=False):
+        commands_to_run = [
+            '/usr/bin/ssh',
+            '-t',
+            self.username + '@' + self.hostname,
+        ]
+
+        if sudo:
+            commands_to_run += ['sudo', '-p "sudo password: "']
+
+        commands_to_run += commands
+
+        return self.run(commands_to_run)
+
+    def copy_to(self, src, dest):
+
+        if os.path.dirname(dest):
+            self.run_command('mkdir -p ' + os.path.dirname(dest))
+
+        return self.run([
+            '/usr/bin/scp',
+            src,
+            self.username + '@' + self.hostname + ':' + dest,
+        ])
+
+    def copy_from(self, src, dest):
+        mkdir_p(os.path.dirname(dest))
+        return self.run([
+            '/usr/bin/scp',
+            self.username + '@' + self.hostname + ':' + src,
+            dest,
+        ])
+
+    def run(self, commands):
         logging.debug('Running "' + ' '.join(commands) + '"')
 
         pid, child_fd = pty.fork()
 
-        def is_child_pid(p):
-            return not p
-
-        if is_child_pid(pid):
+        if pid == CHILD:
             os.execv(commands[0], commands)
+
+        self._commands = commands
+        self._pid = pid
+        self._child_fd = child_fd
+        self._returncode = None
+        self._exception = None
+        self._error = None
+
+        return self
+
+
+    @property
+    def command(self):
+        return ' '.join(self._commands)
+
+    @property
+    def output(self):
 
         password = ''
         password_attempted = False
 
-        output = []
         while True:
-
             try:
-                r = os.read(child_fd, 1024).strip()
-                wpid, wret, wres = os.wait4(pid, os.WNOHANG)
-            except Exception:
+                r = os.read(self._child_fd, 1024).strip()
+                wpid, wret, wres = os.wait4(self._pid, os.WNOHANG)
+                if wpid:
+                    self._returncode = wret
+            except Exception as e:
                 break
             lower = r.lower()
 
             if b'are you sure you want to continue connecting' in lower:
                 logging.debug('Adding host to known hosts')
-                os.write(child_fd, b'yes\n')
+                os.write(self._child_fd, b'yes\n')
 
             elif b'sudo password:' in lower:
                 password = getpass.getpass(prompt='Password for sudo: ')
 
                 logging.debug('Sending sudo password')
-                os.write(child_fd, password.encode('utf-8') + b'\n')
+                os.write(self._child_fd, password.encode('utf-8') + b'\n')
 
             elif b'password:' in lower:
                 if self._password:
@@ -160,21 +154,38 @@ class Ssh:
                     password_attempted = True
 
                 logging.debug('Sending SSH password')
-                os.write(child_fd, password.encode('utf-8') + b'\n')
+                os.write(self._child_fd, password.encode('utf-8') + b'\n')
 
             elif r:
-                output.append(r.decode('utf-8') + '\n')
+                yield r.decode('utf-8')
 
             if wret:
-                err = '\n'.join(output)
-                logging.error(err)
-                return '', err
+                self._error = ['return code: ' + str(wret)]
+                break
 
             if password_attempted and password and lower and b'uthentication fail' not in lower:
                 logging.debug('SSH authenticated')
                 self._password = password
                 password_attempted = False
 
-        out = ''.join(output)
-        logging.log(log_level, out)
-        return out, ''
+    @property
+    def returncode(self):
+        try:
+            pid, ret = os.wait()
+            return ret
+        except:
+            if self._returncode is None:
+                return len(self._error)
+            return self._returncode
+
+    @property
+    def error(self):
+        try:
+            pid, ret = os.wait()
+            if pid:
+                self._returncode = ret
+        except:
+            pass
+        if self._exception:
+            return str(self._exception)
+        return self._error
