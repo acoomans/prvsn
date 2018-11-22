8a2fd8d780f117165fa675d76ea9d4d662010afa
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-21 22:25:20 -0500

    Move to separate chown task

--- prvsnlib/tasks/file.py
+++ prvsnlib/tasks/file.py
@@ -13,12 +13,10 @@ class FileAction:
 
 class FileTask(Task):
 
-    def __init__(self, source, dest, replacements, owner, group, action, secure):
+    def __init__(self, source, dest, replacements, action, secure):
         Task.__init__(self, secure)
         self._source = source
         self._dest = dest
-        self._owner = owner
-        self._group = group
         self._action = action
         self._replacements = replacements
 
@@ -29,28 +27,49 @@ class FileTask(Task):
         out, err = [], []
         mkdir_p(os.path.dirname(self._dest))
         if self._source:
-            o, e = get_replace_write_file(self._source,
+            out, err = get_replace_write_file(self._source,
                                           os.path.join(self._role.path, 'files'),
                                           self._replacements,
                                           self._dest)
-            out += o
-            err += e
+        return TaskResult(output=out, error=err)
+
+
+class ChownTask(Task):
+
+    def __init__(self, path, owner, group, recursive):
+        Task.__init__(self)
+        self._path = path
+        self._owner = owner
+        self._group = group
+        self._recusive = recursive
+
+    def __str__(self):
+        return 'Change ownership of file "' + self._path + '"'
+
+    def run(self):
         try:
             uid = pwd.getpwnam(self._owner).pw_uid if self._owner else -1
             gid = grp.getgrnam(self._group).gr_gid if self._group else -1
-            os.chown(self._dest, uid, gid)
-        except OSError as e:
-            print(uid)
-            print(gid)
-            err.append('Could not change file ownership.')
 
-        return TaskResult(output=out, error=err)
+            out, err = [], []
+            if self._recusive:
+                for root, dirs, files in os.walk(self._path):
+                    for d in dirs:
+                        os.chown(os.path.join(root, d), uid, gid)
+                    for f in files:
+                        os.chown(os.path.join(root, f), uid, gid)
+            return TaskResult(output='changed.')
+        except OSError as e:
+            return TaskResult(error=str(e))
 
 
 def file(source, dest, replacements=None, owner=None, group=None, action=FileAction.ADD, secure=False):
     if replacements is None:
         replacements = {}
-    FileTask(source, dest, replacements, owner, group, action, secure)
+    FileTask(source, dest, replacements, action, secure)
+    if owner or group:
+        ChownTask(dest, owner, group, recursive=False)
 
-def chown(file, owner=None, group=None):
-    FileTask(None, file, None, owner, group, None, False)
+
+def chown(path, owner=None, group=None, recursive=True):
+    ChownTask(path, owner, group, recursive)
