b05102067d23688dad43ddee14543b6468c6e3d4
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-21 19:43:17 -0500

    Restored Process tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -18,40 +18,40 @@ class TestRun(unittest.TestCase):
         l = ''.join([x for x  in gen])
         self.assertTrue(v in l, msg)
 
-    # def testProcessSimpleOutput(self):
-    #     p = Process(['echo', 'hello']).run()
-    #     self.assertJoinedEqual(p.command, 'echo hello', 'incorrect command')
-    #     self.assertJoinedEqual(p.output, 'hello', 'incorrect output')
-    #     self.assertEqual(p.returncode, 0, 'incorrect return')
-    #     self.assertIsNone(p.error, 'incorrect error')
-    #
-    # def testProcessMultilineOutput(self):
-    #     p = Process(['echo', 'hello\nbye']).run()
-    #     self.assertJoinedEqual(p.command, 'echo hello\nbye', 'incorrect command')
-    #     self.assertJoinedEqual(p.output, 'hellobye', 'incorrect output')
-    #     self.assertEqual(p.returncode, 0, 'incorrect return')
-    #     self.assertIsNone(p.error, 'incorrect error')
-    #
-    # def testProcessStdin(self):
-    #     p = Process(['cat'], stdin='hello\nwhatsup\nbye').run()
-    #     self.assertJoinedEqual(p.command, '(cat) hello(cat) whatsup(cat) bye', 'incorrect command')
-    #     self.assertJoinedEqual(p.output, 'hellowhatsupbye', 'incorrect output')
-    #     self.assertEqual(p.returncode, 0, 'incorrect return')
-    #     self.assertIsNone(p.error, 'incorrect error')
-    #
-    # def testProcessErrorReturn(self):
-    #     p = Process(['test', '-n', '']).run()
-    #     self.assertJoinedEqual(p.command, 'test -n ', 'incorrect command')
-    #     self.assertJoinedEqual(p.output, '', 'incorrect output')
-    #     self.assertEqual(p.returncode, 1, 'incorrect return')
-    #     self.assertIsNone(p.error, 'incorrect error')
-    #
-    # def testProcessInvalidCommand(self):
-    #     p = Process(['fjksdhfkljsahfjshaf']).run()
-    #     self.assertJoinedEqual(p.command, 'fjksdhfkljsahfjshaf', 'incorrect command')
-    #     self.assertInJoinedEqual(p.output, 'No such file or directory', 'incorrect output')
-    #     self.assertEqual(p.returncode, 1, 'incorrect return')
-    #     self.assertIsNotNone(p.error, 'incorrect error')
+    def testProcessSimpleOutput(self):
+        p = Process(['echo', 'hello']).run()
+        self.assertJoinedEqual(p.command, 'echo hello', 'incorrect command')
+        self.assertJoinedEqual(p.output, 'hello', 'incorrect output')
+        self.assertEqual(p.returncode, 0, 'incorrect return')
+        self.assertIsNone(p.error, 'incorrect error')
+
+    def testProcessMultilineOutput(self):
+        p = Process(['echo', 'hello\nbye']).run()
+        self.assertJoinedEqual(p.command, 'echo hello\nbye', 'incorrect command')
+        self.assertJoinedEqual(p.output, 'hellobye', 'incorrect output')
+        self.assertEqual(p.returncode, 0, 'incorrect return')
+        self.assertIsNone(p.error, 'incorrect error')
+
+    def testProcessStdin(self):
+        p = Process(['cat'], stdin='hello\nwhatsup\nbye').run()
+        self.assertJoinedEqual(p.command, '(cat) hello(cat) whatsup(cat) bye', 'incorrect command')
+        self.assertJoinedEqual(p.output, 'hellowhatsupbye', 'incorrect output')
+        self.assertEqual(p.returncode, 0, 'incorrect return')
+        self.assertIsNone(p.error, 'incorrect error')
+
+    def testProcessErrorReturn(self):
+        p = Process(['test', '-n', '']).run()
+        self.assertJoinedEqual(p.command, 'test -n ', 'incorrect command')
+        self.assertJoinedEqual(p.output, '', 'incorrect output')
+        self.assertEqual(p.returncode, 1, 'incorrect return')
+        self.assertIsNone(p.error, 'incorrect error')
+
+    def testProcessInvalidCommand(self):
+        p = Process(['fjksdhfkljsahfjshaf']).run()
+        self.assertJoinedEqual(p.command, 'fjksdhfkljsahfjshaf', 'incorrect command')
+        self.assertInJoinedEqual(p.output, 'No such file or directory', 'incorrect output')
+        self.assertEqual(p.returncode, 1, 'incorrect return')
+        self.assertIsNotNone(p.error, 'incorrect error')
 
     def testProcess2SimpleOutput(self):
         p = Process2(['/bin/echo', 'hello']).run()
