20c6ec6d966a170de2a33c7456869ebac11e79ed
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-21 21:21:29 -0500

    Added user and group to file

--- prvsnlib/tasks/file.py
+++ prvsnlib/tasks/file.py
@@ -1,5 +1,7 @@
+import grp
 import os
 import pwd
+
 
 from prvsnlib.utils.file import mkdir_p, get_replace_write_file
 from ..task import Task, TaskResult
@@ -11,11 +13,12 @@ class FileAction:
 
 class FileTask(Task):
 
-    def __init__(self, source, dest, owner, replacements, action, secure):
+    def __init__(self, source, dest, replacements, owner, group, action, secure):
         Task.__init__(self, secure)
         self._source = source
         self._dest = dest
         self._owner = owner
+        self._group = group
         self._action = action
         self._replacements = replacements
 
@@ -31,7 +34,7 @@ class FileTask(Task):
 
         try:
             uid = pwd.getpwnam(self._owner).pw_uid if self._owner else -1
-            gid = pwd.getgrnam(self._group).gr_gid if self._group else -1
+            gid = grp.getgrnam(self._group).gr_gid if self._group else -1
             os.chown(self._dest, uid, gid)
         except OSError as e:
             err.append('Could not change file ownership.')
@@ -39,7 +42,7 @@ class FileTask(Task):
         return TaskResult(output=out, error=err)
 
 
-def file(source, dest, replacements=None, owner=None, action=FileAction.ADD, secure=False):
+def file(source, dest, replacements=None, owner=None, group=None, action=FileAction.ADD, secure=False):
     if replacements is None:
         replacements = {}
-    FileTask(source, dest, replacements, action, secure)
+    FileTask(source, dest, replacements, owner, group, action, secure)
