09db679a4fd22c90a3a1679d894c74ac6d6a7a16
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-11-23 18:39:11 -0500

    Changed validation messages

--- prvsnlib/utils/validation.py
+++ prvsnlib/utils/validation.py
@@ -10,28 +10,28 @@ def log_error_and_exit(message):
 
 def validate_runbook(runbook):
     if not os.path.isdir(runbook.path):
-        log_error_and_exit('Argument "' + runbook.path + '" is not a valid runbook path.')
+        log_error_and_exit('Not a valid runbook: ' + runbook.path)
     return True
 
 
 def validate_roles(runbook, roles):
     if not type(roles) is str:
-        log_error_and_exit('Argument "' + roles + '" is not a valid roles list.')
+        log_error_and_exit('Not valid roles: ' + roles)
     if not roles:
         log_error_and_exit('No roles specified. Nothing to do.')
     for role in roles.split(','):
         if role not in [r.name for r in runbook.roles]:
-            log_error_and_exit('Argument "' + role + '" is not a valid role in runbook.')
+            log_error_and_exit('Not a valid role in runbook: ' + role)
     return True
 
 
 def validate_username(username):
     if not username or not type(username) is str:
-        log_error_and_exit('Argument "' + username + '" is not a valid username.')
+        log_error_and_exit('Not a valid username: ' + username)
     return True
 
 
 def validate_hostname(hostname):
     if not hostname or not type(hostname) is str:
-        log_error_and_exit('Argument "' + hostname + '" is not a valid hostname.')
+        log_error_and_exit('Not a valid hostname: ' + hostname)
     return True
