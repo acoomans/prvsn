634f19e7b14f70b0134450201cf60ebf355ab021
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 19:15:29 +0000

    Adding tarfile member sanitization to extractall()

--- prvsnlib/tasks/unarchive.py
+++ prvsnlib/tasks/unarchive.py
@@ -52,7 +52,26 @@ def untar(src, dest, owner=None, group=None, action=UnarchiveAction.EXTRACT):
             src = os.path.join(context.role.path, 'files', src)
 
         with tarfile.open(src) as tar:
-            tar.extractall(dest)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, dest)
 
         if owner or group:
             chown(dest, owner, group, recursive=True)
